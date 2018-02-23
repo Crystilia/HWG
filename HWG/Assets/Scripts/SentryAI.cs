3f9cb0af6b8660d6dfccfb30ea9bde7c31440818
Cryoxene <cryoxene@gmail.com>
2018-02-23 01:49:35 -0500

    Unity Upload

---
+++ HWG/Assets/Scripts/SentryAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SentryAI : MonoBehaviour {
+
+	PickUpController PickUpController;
+
+	private NavMeshAgent agent;
+	public Transform player;
+	public Transform tree;
+	public bool holding;
+
+
+	void Start () {
+
+		PickUpController = GameObject.Find("Player").GetComponent<PickUpController>();
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+
+		holding = PickUpController.carrying;
+
+		if(holding) {
+			agent.SetDestination(tree.position);
+		}
+		else {
+			agent.SetDestination(player.position);
+		}
+	}
+}
