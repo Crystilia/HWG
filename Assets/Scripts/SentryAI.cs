7d801a44985ba654511c096205d96c83d853d9b6
Cryoxene <cryoxene@gmail.com>
2018-02-26 15:25:45 -0500

    Sentry Updates

--- Assets/Scripts/SentryAI.cs
+++ Assets/Scripts/SentryAI.cs
@@ -7,15 +7,23 @@ public class SentryAI : MonoBehaviour {
 
 	PickUpController PickUpController;
 
+	GameObject sentryCamera;
+	GameObject carriedObject;
 	private NavMeshAgent agent;
 	public Transform player;
-	public Transform tree;
+	public Transform sphere;
+	public Transform dropSpot;
+	public bool carrying;
 	public bool holding;
+	public float distance;
+	public float smooth;
+	public float throwForce = 10;
 
 
 	void Start () {
 
-		PickUpController = GameObject.Find("Player").GetComponent<PickUpController>();
+		sentryCamera = GameObject.FindWithTag("SentryCamera");
+		PickUpController = GameObject.Find("PlayerCamera").GetComponent<PickUpController>();
 		agent = GetComponent<NavMeshAgent>();
 	}
 
@@ -24,10 +32,49 @@ public class SentryAI : MonoBehaviour {
 		holding = PickUpController.carrying;
 
 		if(holding) {
-			agent.SetDestination(tree.position);
-		}
-		else {
 			agent.SetDestination(player.position);
 		}
+		else if (carrying) {
+			carry(carriedObject);
+			agent.SetDestination(dropSpot.position);
+
+			if (!agent.pathPending)
+			{
+				if (agent.remainingDistance <= agent.stoppingDistance)
+				{
+					if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+					{
+						dropItem();
+					}
+				}
+			}
+		}
+		else {
+			agent.SetDestination(sphere.position);
+		}
+	}
+
+	void OnTriggerEnter () {
+
+		Pickupable p = sphere.GetComponent<Pickupable>();
+		if(p != null) {
+			carrying = true;
+			Debug.Log("Agent Carrying");
+			carriedObject = p.gameObject;
+			p.GetComponent<Rigidbody>().isKinematic = true;
+	}
+}
+
+	void carry (GameObject carriedObject) {
+
+		carriedObject.transform.position = Vector3.Lerp(carriedObject.transform.position, sentryCamera.transform.position + sentryCamera.transform.forward * distance, Time.deltaTime * smooth);
+	}
+
+	void dropItem() {
+
+		carrying = false;
+		carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+		carriedObject.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * throwForce;
+		carriedObject = null;
 	}
 }
